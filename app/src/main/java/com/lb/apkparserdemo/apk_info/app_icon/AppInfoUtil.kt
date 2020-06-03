627a9f2cef4b1e4a5b38057ecd13d01ac01253cc
AndroidDeveloperLB <AndroidDeveloperLB5020914>
2020-06-03 22:36:21 +0300

    Added icon parsing demonstration, when possible. The only ones that are supported are images  VectorDrawables, and Adaptive icons that include them. Also, sadly there are some APK files that have the reources hidden somehow.

--- app/src/main/java/com/lb/apkparserdemo/apk_info/app_icon/AppInfoUtil.kt
+++ app/src/main/java/com/lb/apkparserdemo/apk_info/app_icon/AppInfoUtil.kt
@@ -11,8 +11,6 @@ object AppInfoUtil {
         if (appIconSize > 0)
             return appIconSize
         val activityManager = context.getSystemService<ActivityManager>()
-        //https://console.firebase.google.com/u/0/project/app-manager-cdf2c/crashlytics/app/android:com.lb.app_manager/issues/d5c98cf94a3d87148b915413f2583657?time=last-seven-days&sessionId=5D4B95C60017000214A7E2D1546BCD11_DNE_0_v2
-        //TODO maybe after API 6 it's not needed to use try-catch
         appIconSize = try {
             activityManager.launcherLargeIconSize
         } catch (e: Exception) {
